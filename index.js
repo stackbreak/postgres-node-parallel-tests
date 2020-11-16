7fa9b209fc5d6c40f4b78a8e5048b77ee7764951
Max Stackbreak <stackbreak@ya.ru>
2020-11-17 02:44:07 +0300

    unsafe

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const dbPool = require("./src/db-pool");
 
 const APP_PORT = 3005;
 
-dbPool.connect({
-  host: "localhost",
-  port: 5432,
-  database: "socnet",
-  user: "dockerdev",
-  password: "dockerdev",
-});
-
-app().listen(APP_PORT, () => {
-  console.log(`START ON PORT ${APP_PORT}`);
-});
+dbPool
+  .connect({
+    host: "localhost",
+    port: 5432,
+    database: "socnet",
+    user: "dockerdev",
+    password: "dockerdev",
+  })
+  .then(() => {
+    app().listen(APP_PORT, () => {
+      console.log(`START ON PORT ${APP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
--- src/db-pool.js
+++ src/db-pool.js
@@ -5,6 +5,15 @@ class Pool {
 
   connect(options = {}) {
     this.pool = new pg.Pool(options);
+    return this.pool.query(`select 1 + 1;`);
+  }
+
+  close() {
+    return this.pool.end();
+  }
+
+  query(sql) {
+    return this.pool.query(sql);
   }
 }
 
